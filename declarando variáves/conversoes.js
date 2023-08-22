deab450f5eba5f298a8ac03999b4c7e84d1d7975
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-08-22 19:46:24 -0300

    Create conversoes.js

---
+++ declarando variáves/conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita
+const numero = 456
+const numeroString = "456"
+
+
+// Number()
+// String()
+
+//console.log(numero == numeroString)
+//console.log(numero + numeroString)
+console.log(numero + Number(numeroString))
+
+// conservação explícita
