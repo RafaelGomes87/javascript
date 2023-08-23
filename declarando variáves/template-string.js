7f54a650d45a4a6780195446c1d80581eb494656
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-08-23 19:51:18 -0300

    tem

---
+++ declarando variáves/template-string.js
@@ -0,0 +1,22 @@
+// const nome = "Rafael";
+// const idade = 2021-1987;
+// const cidadeDeNascimento = "São Luis";
+
+// //const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+// const apresentacao = `meu nome é ${nome}, 
+// minha idadae é ${idade}
+// e nasci na cidade de ${cidadeDeNascimento}`
+
+
+// console.log(apresentacao)
+
+const nome = "Leo";
+const idade = 17;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
+
+
+
