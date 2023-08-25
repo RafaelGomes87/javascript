38fe2e876f6593dfd88cb4c25f587e0066e3fbdd
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-08-25 20:26:38 -0300

    parametros

---
+++ declarando variáves/parametros.js
@@ -0,0 +1,44 @@
+// parâmatros de funções 
+                //2     //2
+// function soma(numero1, numero2){
+//   return numero1 + numero2;  
+// }
+
+
+// console.log (soma(2,2))
+// console.log (soma(245,20))
+// console.log (soma(-500,60))
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros 
+
+// function nomeIdade (nome, idade){
+//     return `meu nome é ${nome} e minha idade é ${idade}`;
+// }
+
+// //console.log(nomeIdade(40, "Rafael"))
+
+// function soma(numero1, numero2){
+//   return numero1 + numero2;  
+// }
+
+// function multiplica(numero1 = 1, numero2 =1){
+//     return numero1 * numero2;
+// }
+//                                //9     //6
+// // console.log (multiplica(soma(4, 5),soma(3,3)))
+
+// console.log (multiplica(soma(4, 5)))
+
+function operacaoMatematica(numero1, numero2, numero3) {
+    return numero1 + numero2 + numero3
+   }
+   
+   console.log (operacaoMatematica(15, 30, 45)) // 90
+
+
+
+
+
+
