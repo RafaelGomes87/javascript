dccafa3dd8b8d9ebbd27cc41453e9c47ca4acf35
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-09-15 08:29:38 -0300

    atualizado-lista

---
+++ JS-ARRAYS/atualizado-lista.js
@@ -0,0 +1,11 @@
+// const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+// nomes.splice(1,2);
+// nomes.push("Rafael")
+
+// console.log(nomes);
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
