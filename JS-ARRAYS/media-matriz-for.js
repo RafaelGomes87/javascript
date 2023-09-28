53469a1c1492a64e20a903e23412c69a546788d9
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-09-28 20:02:47 -0300

    js

--- JS-ARRAYS/media-matriz-for.js
+++ JS-ARRAYS/media-matriz-for.js
@@ -1,6 +1,6 @@
-const notas1 = [10, 6.5, 8, 7,5] 
-const notas2 = [9, 6, 6]
-const notas3 = [8.5, 9.5]
+const notas1 = [10, 6.5, 8, 7.5] ;
+const notas2 = [9, 6, 6];
+const notas3 = [8.5, 9.5];
 
 const notasGerais = [notas1,notas2,notas3]
 
---
+++ JS-ARRAYS/possibilidade-for.js
@@ -0,0 +1,6 @@
+const numerosPares = [];
+
+for (let i = 2; i <= 100; i += 2) {
+    numerosPares.push (i);
+}
+    console.log(numerosPares);
