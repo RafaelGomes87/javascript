cb104b665a91f4cea8058b8ed8ddcb8284f66f03
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-10-02 20:48:58 -0300

    set

---
+++ JS-ARRAYS/Set.js
@@ -0,0 +1,12 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+
+const meuSet = new Set(nomes);
+
+const nomesAtualizados = [...meuSet];
+
+
+console.log(nomesAtualizados);
+
+
+
