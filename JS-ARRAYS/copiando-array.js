25f3bfa444aa7ccecdb781f294bc0b4c8ac87394
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-10-02 20:41:05 -0300

    copiando-array

---
+++ JS-ARRAYS/copiando-array.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+const novasNotas = [...notas, 10];
+
+
+
+console.log (`As novas notas são ${novasNotas}`);
+console.log (`As notas original são ${notas}`);
---
+++ JS-ARRAYS/index-reduce.js
@@ -0,0 +1,5 @@
+const  numeros = [43, 50, 65, 12];
+
+const  soma = numeros.reduce((acc, atual)=> atual + acc, 0)
+
+console.log (soma)
