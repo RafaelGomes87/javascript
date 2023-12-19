db7775bcdc5f1e837ad0db7142a396b92d8bee4e
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2023-12-19 10:29:42 -0300

    update

--- algoritmo-javascript/selectionScort.js
+++ algoritmo-javascript/selectionScort.js
@@ -2,7 +2,7 @@ const livros = require('./lista-livros');
 
 const menorValor = require('./menorValor');
 
-for (let atual = 0; atual < livros.length; atual++){
+for (let atual = 0; atual < livros.length -1 ; atual++){
     let menor = menorValor(livros, atual)
     
     let livroAtual = livros[atual];
