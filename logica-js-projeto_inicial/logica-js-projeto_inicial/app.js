ad0f9d993aec12a01783c3cdd49a97f3a1432126
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-03 11:27:46 -0300

    j

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -2,14 +2,16 @@ alert ('Boas vindas ao jogo do número secreto');
 
 let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute 
+let chute;
+let tentativas = 1;
+
 // enquanto chute não for igual ao n. s. 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');   
   
     if (chute == numeroSecreto ) {
         
-        alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} `);
+        alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     // se chute for igural ao número secreto  
     } else {
         if (chute > numeroSecreto){
@@ -17,6 +19,9 @@ while (chute != numeroSecreto) {
             } else {
                 alert (`O numero secreto é maior que ${chute}`);
         }
+        //tentativas = tentativas + 1;
+        
+        tentativas++;
     }   
 }
 
