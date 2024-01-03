48e0730cd6846dac3b12abdfed6f46d5cbe52253
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-03 10:40:06 -0300

    logica de javascript

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,14 +1,24 @@
 alert ('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
-
-
-if (chute == numeroSecreto ) {
-    console.log
-    alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} `);
- 
-} else {
-    alert(`Você erro, número escolhido no chute foi ${chute}`)
+let chute 
+// enquanto chute não for igual ao n. s. 
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');   
+  
+    if (chute == numeroSecreto ) {
+        
+        alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} `);
+    // se chute for igural ao número secreto  
+    } else {
+        if (chute > numeroSecreto){
+            alert(`O numero secreto é manor que ${chute}`);
+            } else {
+                alert (`O numero secreto é maior que ${chute}`);
+        }
+    }   
 }
+
+
+
