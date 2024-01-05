31a4971da5b4814dbb62d076b2c6e2b39553a89c
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-05 10:40:24 -0300

    [

---
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número sercreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entra 1 e 10';
+
+function verificarChute(){
+      console.log ('o batão foi clicado!')
+}
