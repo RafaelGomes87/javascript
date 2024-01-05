e94973db204046a19660269f64fdfd9056021baf
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-05 10:16:29 -0300

    jogos

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,17 +1,18 @@
 alert ('Boas vindas ao jogo do número secreto');
-
-let numeroSecreto = 5;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() *  numeroMaximo + 1);
 console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
 // enquanto chute não for igual ao n. s. 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');   
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);   
   
     if (chute == numeroSecreto ) {
         
-        alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break; 
+       
     // se chute for igural ao número secreto  
     } else {
         if (chute > numeroSecreto){
@@ -24,6 +25,16 @@ while (chute != numeroSecreto) {
         tentativas++;
     }   
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1 ){
+//     alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else  {
+//     alert (`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+
+// }
 
 
 
