2b945f29cf0dfd2aad71ce4a5021be7b23255ec2
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-08 20:06:26 -0300

    jogo secreto

--- js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,9 +1,49 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número sercreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entra 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+function exibirTextoNaTela(tag, texto){
+      let campo = document.querySelector(tag);
+      campo.innerHTML = texto;
+}
+ function exibirMensagemInicial(){
+      exibirTextoNaTela('h1','Jogo do número secreto');
+      exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+ }
+exibirMensagemInicial();
 
 function verificarChute(){
-      console.log ('o batão foi clicado!')
+      let chute = document.querySelector('input').value;
+      if (chute == numeroSecreto){
+            exibirTextoNaTela('h1', 'Acertou!');
+            let palavraTentativa = tentativas > 1 ? 'tentativas':'tentativa'
+            let mensgemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+            exibirTextoNaTela('p', mensgemTentativas);
+            document.getElementById('reiniciar').removeAttribute('disabled');
+      
+      } else{
+            if(chute > numeroSecreto){
+                  exibirTextoNaTela('p', 'O número secreto é menor');
+            } else{
+                  exibirTextoNaTela('p', 'O número secreto é maior');
+            }
+            tentativas++;
+            limparCampo();
+      }
+
 }
+
+function gerarNumeroAleatorio(){
+      return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+      chute = document.querySelector('input');
+      chute.value = '';
+}
+ function reiniciarJogo() {
+      numeroSecreto = gerarNumeroAleatorio();
+      limparCampo();
+      tentativas = 1;
+      exibirMensagemInicial();
+      document.getElementById('reiniciar').setAttribute('disabled', true)
+      
+ }
