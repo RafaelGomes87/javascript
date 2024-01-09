a2e93811274ef9a8350f4ae2fe192a7a3694237f
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-09 09:59:43 -0300

    jogo

--- js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,8 +1,14 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+
+
+
 function exibirTextoNaTela(tag, texto){
       let campo = document.querySelector(tag);
       campo.innerHTML = texto;
+      responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2});
 }
  function exibirMensagemInicial(){
       exibirTextoNaTela('h1','Jogo do número secreto');
@@ -32,7 +38,20 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-      return parseInt(Math.random() * 10 + 1);
+      let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+      let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+      if (quantidadeDeElementosNaLista == numeroLimite){
+            listaDeNumerosSorteados = [];
+      }
+
+      if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+         return gerarNumeroAleatorio();  
+      } else {
+            listaDeNumerosSorteados.push(numeroEscolhido);
+            console.log(listaDeNumerosSorteados);
+            return numeroEscolhido;
+      }
 }
 
 function limparCampo() {
@@ -45,5 +64,5 @@ function limparCampo() {
       tentativas = 1;
       exibirMensagemInicial();
       document.getElementById('reiniciar').setAttribute('disabled', true)
-      
+    
  }
