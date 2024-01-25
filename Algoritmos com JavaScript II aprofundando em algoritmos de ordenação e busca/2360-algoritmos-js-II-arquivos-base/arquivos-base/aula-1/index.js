2b60d4659070919a5369d35e5babeaf61dc7f4cb
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-01-25 11:08:13 -0300

    aula 1

---
+++ Algoritmos com JavaScript II aprofundando em algoritmos de ordenação e busca/2360-algoritmos-js-II-arquivos-base/arquivos-base/aula-1/index.js
@@ -0,0 +1,28 @@
+const {edGalho, edFolha} = require('./arrays');
+
+function juntalistas(lista1, lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+    let atual = 0;
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+        
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco){
+          listaFinal[atual] = produtoAtualLista1;
+          posicaoAtualLista1++;  
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+          posicaoAtualLista2++;  
+        }
+        
+        atual++;
+    }
+        
+    
+    return listaFinal;
+}
+
+console.log(juntalistas(edGalho, edFolha))
