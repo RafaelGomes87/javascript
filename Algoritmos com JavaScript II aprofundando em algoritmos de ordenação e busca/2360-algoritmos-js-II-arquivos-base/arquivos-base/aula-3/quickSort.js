830dc1cf9e802168746dc081f705da79ea91626f
Rafael Gomes <106036087+RafaelGomes87@users.noreply.github.com>
2024-03-06 20:56:10 -0300

    quick

--- Algoritmos com JavaScript II aprofundando em algoritmos de ordenação e busca/2360-algoritmos-js-II-arquivos-base/arquivos-base/aula-3/quickSort.js
+++ Algoritmos com JavaScript II aprofundando em algoritmos de ordenação e busca/2360-algoritmos-js-II-arquivos-base/arquivos-base/aula-3/quickSort.js
@@ -3,10 +3,14 @@ const listaLivros = require('./array');
 function quickSort(arry, esqueda, direita) {
 
  if (arry.length > 1){
-
+    let varTemp = particiona(array, esqueda, direita);
  }   
 
 }
 return array;
+
+function particiona(array, esqueda, direita){
+    let pivo = array[Math.floor((esqueda + direita)/ 2)]
+}
 
 console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
