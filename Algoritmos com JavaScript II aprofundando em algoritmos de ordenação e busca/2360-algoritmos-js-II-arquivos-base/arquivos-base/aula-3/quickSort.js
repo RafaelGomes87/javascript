258a14e1276a62e9f8911e266941ea65c2c1edd3
Rafael Gomes <106036087+RafaelGomes87@users.noreply.github.com>
2024-03-06 21:24:47 -0300

    quick

--- Algoritmos com JavaScript II aprofundando em algoritmos de ordenação e busca/2360-algoritmos-js-II-arquivos-base/arquivos-base/aula-3/quickSort.js
+++ Algoritmos com JavaScript II aprofundando em algoritmos de ordenação e busca/2360-algoritmos-js-II-arquivos-base/arquivos-base/aula-3/quickSort.js
@@ -1,16 +1,44 @@
 const listaLivros = require('./array');
+const trocaLugar = require('./encontraMenores');
 
-function quickSort(arry, esqueda, direita) {
+function quickSort(array, esquerda, direita) {
 
- if (arry.length > 1){
-    let varTemp = particiona(array, esqueda, direita);
+ if (array.length > 1){
+    let indiceAtual = particiona(array, esquerda, direita);
+    if (esquerda <indiceAtual - 1){
+        quickSort(array, esquerda, indiceAtual - 1);
+     }
+    if(indiceAtual < direita){
+        quickSort(array, indiceAtual, direita)
+     }
  }   
-
+ return array;
 }
-return array;
 
-function particiona(array, esqueda, direita){
-    let pivo = array[Math.floor((esqueda + direita)/ 2)]
+
+function particiona(array, esquerda, direita){
+    console.log('array',array)
+    console.log('esquerda, direita',esquerda, direita)
+    
+    let pivo = array[Math.floor((esquerda + direita)/ 2)]
+    let atualEsquerda = esquerda; //0
+    let atualDireita = direita; //10
+    
+    while (atualEsquerda <= atualDireita){
+        while(array[atualEsquerda].preco < pivo.preco){
+            atualEsquerda++
+        }
+        while(array[atualDireita].pivo > pivo.preco){
+            atualDireita--
+
+        }
+        if (atualEsquerda <= atualDireita){
+            trocaLugar(array,atualEsquerda, atualDireita );
+            atualEsquerda++;
+            atualDireita--;
+        }
+    }
+    return atualEsquerda;
 }
 
 console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
