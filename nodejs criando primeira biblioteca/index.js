b0babd8e2b3503546fd99fade05fa586cfc820e8
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-12 14:05:27 -0300

    promessas

--- nodejs criando primeira biblioteca/index.js
+++ nodejs criando primeira biblioteca/index.js
@@ -1,19 +1,27 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function tratErro(erro){
+function trataErro(erro){
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretótrio')); // esta funcição é para localizar o erro no codigo
 }
 
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro,texto) => {
-        if (erro) {
-            tratErro(erro);
-        }
-        console.log(chalk.green(texto));
-    } )
+    fs.promises
+     .readFile(caminhoDoArquivo, encoding)
+     .then((texto) => console.log(chalk.green(texto)))
+     .catch(trataErro)
 }
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro,texto) => {
+//         if (erro) {
+//             tratErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     } )
+// }
 
 pegaArquivo('./arquivos/');
