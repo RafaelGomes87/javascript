6fd7fd42022bc0802d3ef1a72aab15b1f29680a7
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-14 16:45:14 -0300

    retornando os links

--- nodejs criando primeira biblioteca/index.js
+++ nodejs criando primeira biblioteca/index.js
@@ -1,15 +1,12 @@
 import fs from 'fs';
 import chalk from 'chalk';
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
 function extraiLinks(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = regex.exec(texto);
-    console.log(capturas);
-
+    const capturas = [...texto.matchAll(regex)]; // os tres pontinho é para espalha o array
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
 }
-
-extraiLinks(textoTeste);
 
 
 
@@ -24,7 +21,7 @@ function trataErro(erro){
     try {
         const encoding ='utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green (texto));
+        console.log(extraiLinks(texto));
     } catch (erro){
         trataErro(erro);
     }
@@ -50,6 +47,6 @@ function trataErro(erro){
 //     } )
 // }
 
-//pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto.md');
 
 
