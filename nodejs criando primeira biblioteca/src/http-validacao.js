efd7857539f2fcdecf6747bc0b2118cf96728330
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-18 14:12:12 -0300

    Validando links

--- nodejs criando primeira biblioteca/src/http-validacao.js
+++ nodejs criando primeira biblioteca/src/http-validacao.js
@@ -2,6 +2,25 @@ function extraiLinks (arrLinks) {
    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
-export default function listaValidada (listaDeLink){
-    return extraiLinks(listaDeLink);
+async function chacaStatus (listaURLs){
+    const arrStatus = await Promise
+    .all(
+        listaURLs.map(async (url) => {
+            const response = await fetch(url)
+            return response.status;
+        })
+
+    )
+    return arrStatus;
 }
+
+export default async function listaValidada (listaDeLink){
+    const links = extraiLinks(listaDeLink);
+    const status = await chacaStatus(links);
+   return status;
+}
+
+// [gatinho salsicha](http://gatinhosalsicha.com.br/)
+
+
+
