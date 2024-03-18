73528e28bd90b0f4a0ac0a38b0d7f2055afa3982
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-18 11:35:08 -0300

    trando erros

--- nodejs criando primeira biblioteca/src/cli.js
+++ nodejs criando primeira biblioteca/src/cli.js
@@ -1,10 +1,39 @@
 import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from './index.js';
 
 const caminho = process.argv;
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2]);
-    console.log(chalk.yellow('lista de link'),resultado);
+
+function imprimeLista(resultado, identificador = '') {
+  console.log(
+    chalk.yellow('lista de links'),
+    chalk.black.bgGreen(identificador),
+    resultado);
+}
+
+
+async function processaTexto(argumentos) {
+  const caminho = argumentos[2];
+
+  try {
+    fs.lstatSync(caminho);
+  } catch (erro) {
+    if (erro.code === 'ENOENT') {
+      console.log('arquivo ou diretório não existe');
+      return;
+    }
+  }
+
+  if (fs.lstatSync(caminho).isFile()) {
+    const resultado = await pegaArquivo(argumentos[2]);
+    imprimeLista(resultado);
+  } else if (fs.lstatSync(caminho).isDirectory()) {
+    const arquivos = await fs.promises.readdir(caminho)
+    arquivos.forEach(async (nomeDeArquivo) => {
+      const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+      imprimeLista(lista, nomeDeArquivo)
+    })
+  }
 }
 
 processaTexto(caminho);
--- nodejs criando primeira biblioteca/src/index.js
+++ nodejs criando primeira biblioteca/src/index.js
@@ -1,52 +1,28 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function extraiLinks(texto){
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)]; // os tres pontinho é para espalha o array
-    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados.length !== 0 ? resultados : 'não há lins no arquivo';
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
-
-
-function trataErro(erro){
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretótrio')); // esta funcição é para localizar o erro no codigo
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
 // async/await
 
- async function pegaArquivo(caminhoDoArquivo){
-    try {
-        const encoding ='utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        return extraiLinks(texto);
-    } catch (erro){
-        trataErro(erro);
-    }
- }
-
-// promises com then()
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.promises
-//      .readFile(caminhoDoArquivo, encoding)
-//      .then((texto) => console.log(chalk.green(texto)))
-//      .catch(trataErro)
-// }
-
-// function pegaArquivo(caminhoDoArquivo){
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro,texto) => {
-//         if (erro) {
-//             tratErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     } )
-// }
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
 
 export default pegaArquivo;
-
-
