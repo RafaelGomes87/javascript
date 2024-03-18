af15a2c4c52737c2a42891cf03f6c1aa290f9312
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-18 14:28:51 -0300

    Montando o objeto

--- nodejs criando primeira biblioteca/src/http-validacao.js
+++ nodejs criando primeira biblioteca/src/http-validacao.js
@@ -1,3 +1,5 @@
+import chalk from "chalk";
+
 function extraiLinks (arrLinks) {
    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
@@ -6,21 +8,38 @@ async function chacaStatus (listaURLs){
     const arrStatus = await Promise
     .all(
         listaURLs.map(async (url) => {
-            const response = await fetch(url)
-            return response.status;
+            try {
+                const response = await fetch(url)
+                return response.status;
+
+            } catch (erro) {
+                return manejaErros(erro)
+
+            }
         })
 
     )
     return arrStatus;
 }
 
+function manejaErros (erro) {
+    if (erro.cause.code === 'ENOTFOUND'){
+        return 'link não encontrado';
+    }else{
+        return 'ocorreu um erro';
+    }
+}
+
 export default async function listaValidada (listaDeLink){
     const links = extraiLinks(listaDeLink);
     const status = await chacaStatus(links);
-   return status;
+    
+    return listaDeLink.map((objeto, indice) => ({
+      ...objeto,
+      status: status[indice] 
+    }))
 }
 
-// [gatinho salsicha](http://gatinhosalsicha.com.br/)
 
 
 
