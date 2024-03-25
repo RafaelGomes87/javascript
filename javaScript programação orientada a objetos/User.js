a598995c19d32350cddc104143c8f14ab837e3c7
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-03-25 13:13:43 -0300

    k

--- javaScript programação orientada a objetos/User.js
+++ javaScript programação orientada a objetos/User.js
@@ -1,4 +1,4 @@
- class User {
+ export default class User {
     constructor(nome, email, nascimento, role, ativo = true) {
         this.nome = nome
         this.email = email
@@ -12,12 +12,7 @@
     }
 
  }
-
-
-
- const novoUser = new User('Rafael', 'ra@fa.com','2021-01-01')
- console.log(novoUser),
- console.log(novoUser.exibirInfos())
-
-
- console.log(User.prototype.isPrototypeOf(novoUser)) // true
+//  const novoUser = new User('Rafael', 'ra@fa.com','2021-01-01')
+//  console.log(novoUser),
+//  console.log(novoUser.exibirInfos())
+//  console.log(User.prototype.isPrototypeOf(novoUser)) // true
