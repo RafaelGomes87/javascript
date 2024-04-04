0df13317032e8394c137e4a15c108baaa60a63ec
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-04-04 11:07:50 -0300

    app

--- node js api rest express mongodb/src/app.js
+++ node js api rest express mongodb/src/app.js
@@ -2,8 +2,25 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos Anéis"
+    },
+    {
+        id: 2,
+        titulo: "o Hobbit"
+    }
+]
+
 app.get("/",(req, res) =>{
     res.status(200).send("Curso de Node.js");
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+
 
 export default app;
