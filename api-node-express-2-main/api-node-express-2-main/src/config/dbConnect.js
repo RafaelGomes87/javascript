ea4bf18775f0e4697729afa8a86c497500060fad
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-04-16 14:52:06 -0300

    Node.js: lidando com buscas, filtros, paginação e erros em uma API

---
+++ api-node-express-2-main/api-node-express-2-main/src/config/dbConnect.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose"
+
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.geduiwl.mongodb.net/");
+
+let db = mongoose.connection;
+
+export default db;
---
+++ api-node-express-2-main/api-node-express-2-main/src/controllers/autoresController.js
@@ -0,0 +1,70 @@
+import autores from "../models/Autor.js";
+
+class AutorController {
+
+  static listarAutores = async(req, res) => {
+    try {
+      const autoresResultado = await autores.find();
+
+      res.status(200).json(autoresResultado);
+      
+  } catch (erro) {
+          res.status(500).json({ message: "Erro interno no servidor" });
+  }
+  }
+
+  static listarAutorPorId = async (req, res) => {
+    
+      try {
+        const id = req.params.id;
+  
+        const autorResultado = await autores.findById(id);
+  
+        res.status(200).send(autorResultado);
+      } catch (erro) {
+        res.status(400).send({message: `${erro.message} - Id do Autor não localizado.`});
+      }
+    }
+  
+  
+    static cadastrarAutor = async (req, res) => {
+      try {
+        let autor = new autores(req.body);
+  
+        const autorResultado = await autor.save();
+  
+        res.status(201).send(autorResultado.toJSON());
+      } catch (erro) {
+        res.status(500).send({message: `${erro.message} - falha ao cadastrar Autor.`});
+      }
+    }
+  
+
+    static atualizarAutor = async (req, res) => {
+      try {
+        const id = req.params.id;
+  
+        await autores.findByIdAndUpdate(id, {$set: req.body});
+  
+        res.status(200).send({message: "Autor atualizado com sucesso"});
+      } catch (erro) {
+        res.status(500).send({message: erro.message});
+      }
+    }
+  
+    static excluirAutor = async (req, res) => {
+      try {
+        const id = req.params.id;
+  
+        await autores.findByIdAndDelete(id);
+  
+        res.status(200).send({message: "Autor removido com sucesso"});
+      } catch (erro) {
+        res.status(500).send({message: erro.message});
+      }
+    }
+  
+
+}
+
+export default AutorController
