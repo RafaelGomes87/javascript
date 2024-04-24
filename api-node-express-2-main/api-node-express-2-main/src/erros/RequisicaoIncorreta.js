ea29822909bee7ba21de819b9b8c11ee1219885a
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-04-24 14:21:36 -0300

    Refatorando o manipulador de erros

---
+++ api-node-express-2-main/api-node-express-2-main/src/erros/RequisicaoIncorreta.js
@@ -0,0 +1,10 @@
+import ErroBase from "./ErroBase.js";
+
+class RequisicaoIncorreta extends ErroBase{
+constructor(mensagem = "Um ou mais dados fornecidos estão incorretos"){
+    super(mensagem,400);
+
+}
+}
+
+export default RequisicaoIncorreta;
