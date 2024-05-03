73945fb685d7f13bc61dd3732130c577ce9c8997
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-05-03 11:00:30 -0300

    criando um modelo

--- ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/migrations/20240503125336-create-pessoa.js
+++ ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/migrations/20240503125336-create-pessoa.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Pessoas', {
+    await queryInterface.createTable('pessoas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Pessoas');
+    await queryInterface.dropTable('pessoas');
   }
 };
