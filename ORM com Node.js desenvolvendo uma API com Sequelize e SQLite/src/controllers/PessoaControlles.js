c84d7e5a7a5e04982072206f07e9ff830ab1706f
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-05-06 14:24:08 -0300

    Associando migrações

--- ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/controllers/PessoaControlles.js
+++ ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/controllers/PessoaControlles.js
@@ -1,11 +1,12 @@
 const Controller = require('./Controller.js');
-const PessoaServices = require('../services/PessoaServices.js')
+const PessoaServices = require('../services/PessoaServices.js');
+
 const pessoaServices = new PessoaServices();
 
 class PessoaController extends Controller {
-    constructor() {
-    super(pessoaServices);                
-    }
+  constructor() {
+    super(pessoaServices);
+  }
 }
 
 module.exports = PessoaController;
--- ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/services/PessoaServices.js
+++ ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/services/PessoaServices.js
@@ -1,9 +1,9 @@
 const Services = require('./Services.js');
 
 class PessoaServices extends Services {
-    constructor(){
-        super('Pessoa');
-    }
+  constructor() {
+    super('Pessoa');
+  }
 }
 
 module.exports = PessoaServices;
