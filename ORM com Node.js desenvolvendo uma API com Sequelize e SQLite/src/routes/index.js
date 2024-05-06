89c6d784c294088109d78e812c066dc5c1117aff
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-05-06 16:07:23 -0300

    Adicionando rotas

--- ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/routes/index.js
+++ ORM com Node.js desenvolvendo uma API com Sequelize e SQLite/src/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const pessoas = require('./pessoasRoute.js');
+const categorias = require('./categoriasRoute.js');
+const cursos = require('./cursosRoute.js');
+const CategoriaServices = require('../services/CategoriaServices.js');
 
 module.exports = app => {
   app.use(
     express.json(),
     pessoas,
+    categorias,
+    cursos
     );
 };
