b539f663b9cb123539c61d47d289ef2595338989
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-05-22 17:24:34 -0300

    Testando falhas em transações

--- ORM com Node.js avançando nas funcionalidades do Sequelize II/src/services/PessoaServices.js
+++ ORM com Node.js avançando nas funcionalidades do Sequelize II/src/services/PessoaServices.js
@@ -1,8 +1,10 @@
+const dataSource = require('../database/models');
 const Services = require('./Services.js');
 
 class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
+    this.matriculaServices =  new Services('Matriculas');
   }
 
   async pegaMatriculasAtivasPorEstudante(id) {
@@ -19,6 +21,13 @@ class PessoaServices extends Services {
   async pegaPessoasEscopoTodos () {
     const listaPessoas = await super.pegaRegistrosPorEscopo('todosOsRegistros');
     return listaPessoas;
+  }
+
+  async canceçaPessoaEMatriculas (estudanteId) {
+    return dataSource.sequelize.transaction(async(transacao) => { 
+      await super.atualizaRegistro({ ativo: false }, { id: estudanteId }, transacao);
+      await this.matriculaServices.atualizaRegistro({status: 'cancelado'}, { estudadente_id: estudanteId }, transacao);
+    });
   }
 }
 
