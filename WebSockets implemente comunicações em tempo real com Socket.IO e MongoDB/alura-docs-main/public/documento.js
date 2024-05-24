9f78faf9292e0fa6b3344c687e414df5a16fff0a
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-05-24 10:26:46 -0300

    Emitindo eventos

--- WebSockets implemente comunicações em tempo real com Socket.IO e MongoDB/alura-docs-main/public/documento.js
+++ WebSockets implemente comunicações em tempo real com Socket.IO e MongoDB/alura-docs-main/public/documento.js
@@ -1,1 +1,8 @@
 const socket = io();
+
+ const textoEditor = document.getElementById("editor-texto");
+
+textoEditor.addEventListener("keyup",() => {
+    socket.emit("editor-texto", textoEditor.value )
+    
+});
--- WebSockets implemente comunicações em tempo real com Socket.IO e MongoDB/alura-docs-main/src/socket-back.js
+++ WebSockets implemente comunicações em tempo real com Socket.IO e MongoDB/alura-docs-main/src/socket-back.js
@@ -2,4 +2,9 @@ import io from "./servidor.js";
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
+  
+    socket.on("editor-texto", (texto) => {
+      console.log("oi");
+    });
+  
   });
