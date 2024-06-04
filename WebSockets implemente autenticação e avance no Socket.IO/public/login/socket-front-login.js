4502a8c35cd4492994d0ea512e96bac8ec29c648
Rafael Gomes <106036087+RafaelGomeshtml@users.noreply.github.com>
2024-06-04 13:52:15 -0300

    Autenticando usuários

--- WebSockets implemente autenticação e avance no Socket.IO/public/login/socket-front-login.js
+++ WebSockets implemente autenticação e avance no Socket.IO/public/login/socket-front-login.js
@@ -5,4 +5,12 @@ function emitirAutenticarUsuario(dados) {
 
 }
 
+socket.on("autenticacao_sucesso",() => {
+    alert("Usuário autenticado com sucesso!")
+    window.location.href = "/";
+});
+
+socket.on("autenticacao_erro",() => alert("Erro na autenticação"));
+socket.on("usuario_nao_encontrado",() => alert("Usuário não encontrado."));
+
 export { emitirAutenticarUsuario };
--- WebSockets implemente autenticação e avance no Socket.IO/servidor/registrarEventos/login.js
+++ WebSockets implemente autenticação e avance no Socket.IO/servidor/registrarEventos/login.js
@@ -1,9 +1,22 @@
 import { encontrarUsuario } from "../db/usuariosDb.js";
+import autenticarUsuario from "../utils/autenticarUsuario.js";
 
 function registrarEventosLogin(socket, io) {
  socket.on("autenticar_usuario", async ({ nome, senha }) => {
    const usuario =  await encontrarUsuario(nome);
-    console.log(usuario);
+
+   if (usuario){
+    const autenticado = autenticarUsuario(senha, usuario);
+
+   if (autenticado) {
+    socket.emit("autenticacao_sucesso");
+   } else {
+    socket.emit("autenticacao_erro");
+   }
+
+   } else {
+    socket.emit("usuario_nao_encontrado");
+   }   
  });
 }
 
