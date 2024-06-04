eb4dfec6ac371d15dc6cc2423695b96c94166727
Rafael Gomes <106036087+RafaelGomes87@users.noreply.github.com>
2024-06-04 19:58:56 -0300

    Autorizando usuários

--- WebSockets implemente autenticação e avance no Socket.IO/public/socket-front-index.js
+++ WebSockets implemente autenticação e avance no Socket.IO/public/socket-front-index.js
@@ -1,6 +1,11 @@
 import { inserirLinkDocumento, removerLinkDocumento } from "./index.js";
+import { obterCookie } from "./utils/cookies.js";
 
-const socket = io();
+const socket = io({
+  auth: {
+    token: obterCookie("tokenJwt"),
+  }
+});
 
 socket.on("connect_error", (erro) =>{
   alert(erro);
--- WebSockets implemente autenticação e avance no Socket.IO/servidor/middlewares/autorizarUsuario.js
+++ WebSockets implemente autenticação e avance no Socket.IO/servidor/middlewares/autorizarUsuario.js
@@ -1,5 +1,18 @@
+import jwt from "jsonwebtoken";
+
+
 function autorizarUsuario(socket, next) {
+const tokenJwt = socket.handshake.auth.token;
+
+try{
+    jwt.verify(tokenJwt, process.env.SEGREDo_JWT);
+
     next();
+
+} catch (erro) {
+   next(erro);
+  }
+    
 }
 
 export default autorizarUsuario;
